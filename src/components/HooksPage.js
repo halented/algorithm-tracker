6e8ee1dc87e7777b2913da19fd40a9da17034e46
hbj <heyhal19@gmail.com>
2020-07-28 17:10:38 -0700

    uses async await instead of promise.all

--- src/components/HooksPage.js
+++ src/components/HooksPage.js
@@ -3,20 +3,19 @@ import { services } from '../services'
 
 function HooksPage() {
     const [students, studentChanger] = useState([])
-
     const [aGroup, aChanger] = useState(null)
     const [bGroup, bChanger] = useState(null)
 
     useEffect(runSetup, [])
 
-    function runSetup(){
+    function runSetup() {
         services.fetchData()
-        .then(json=>{
-            studentChanger(json)
-        })
+            .then(json => {
+                studentChanger(json)
+            })
     }
 
-    function picker(){
+    function picker() {
         let studentsWhoHaventGone = students.filter(student => student.have === false)
         const randomNumber = Math.floor((Math.random() * (studentsWhoHaventGone.length)))
         return studentsWhoHaventGone[randomNumber]
@@ -28,37 +27,37 @@ function HooksPage() {
         // second, highlight that student's oval ▼
         let tempState = [...students]
         tempState.forEach(student => {
-          if (student === victim) {
-            student.name === "HAL" ? 
-            student.chosen = "mega" 
-            : 
-            student.chosen = 'highlight'
-          }
+            if (student === victim) {
+                student.name === "HAL" ?
+                    student.chosen = "mega"
+                    :
+                    student.chosen = 'highlight'
+            }
         })
         studentChanger(tempState)
     }
 
-    function twoGroupGenerator(){
+    function twoGroupGenerator() {
         // somewhere in here we need to account for the edge case of only one student being left. once the reset function works, just check for the length of students who haven't gone, and if it's 1 (maybe put that logic in the picker), pick that one student and then do the reset function before continuing
         // first, pick two random students ▼
         let a = picker()
         let b = picker()
-        while(a === b){
+        while (a === b) {
             b = picker()
         }
         // then, split the remaining students intwo two groups (ignoring the two who were selected) ▼
         let one = [a]
         let two = [b]
-        let half = Math.floor(students.length/2)
+        let half = Math.floor(students.length / 2)
 
-        for(let i=0; i<students.length; i++){
+        for (let i = 0; i < students.length; i++) {
             // alternate shuffling idea: go through student array in order, and randomly decide which group they go to (til both have half)
             // ignoring the two selected students
-            if (students[i].id !== a.id && students[i].id !== b.id){
-                if(Math.floor(Math.random()*2) === 0 && one.length<half){
+            if (students[i].id !== a.id && students[i].id !== b.id) {
+                if (Math.floor(Math.random() * 2) === 0 && one.length < half) {
                     one.push(students[i])
                 }
-                else if (two.length<half) {
+                else if (two.length < half) {
                     two.push(students[i])
                 }
                 else {
@@ -71,16 +70,16 @@ function HooksPage() {
     }
 
     const populateStudents = (val) => {
-        let correctSet = [...students].filter(student=> student.have === val)
+        let correctSet = [...students].filter(student => student.have === val)
 
         return correctSet.map(student => {
             return (
-              <li
-                className={`studentLink ${student.chosen}`}
-                onClick={() => swapList(student)}
-                key={student.id}>
-                {student.name}
-              </li>)
+                <li
+                    className={`studentLink ${student.chosen}`}
+                    onClick={() => swapList(student)}
+                    key={student.id}>
+                    {student.name}
+                </li>)
         })
     }
 
@@ -88,100 +87,117 @@ function HooksPage() {
         let replacement;
         student.have = !student.have
         student.chosen = ''
-        replacement = students.map(x=> {
-            if(x.name === student.name){return student}
+        replacement = students.map(x => {
+            if (x.name === student.name) { return student }
             else return x
         })
+
         return services.postData(student)
-        .then(data=>{
-            studentChanger(replacement)
-        })
+            .then(data => {
+                studentChanger(replacement)
+            })
     }
 
-    const doubleSwap = (st1,st2) => {
+    const doubleSwap = (st1, st2) => {
         swapList(st1)
-        .then(rt=>{
-            swapList(st2)
-        })
-        .then(rt=>{
-            aChanger(null)
-            bChanger(null)
-        })
+            .then(rt => {
+                swapList(st2)
+            })
+            .then(rt => {
+                aChanger(null)
+                bChanger(null)
+            })
     }
 
-    const reset = () => {
-      let promArray = []
-      let alteredStudents = students.map(x=>{
-          x.have = false
-          return x
-      })
-      alteredStudents.forEach(x=>promArray.push(services.promiseMakerForReset(x)))
-      Promise.all(promArray)
-      .then(vals=>{
-          studentChanger(alteredStudents)
+    const reset = async () => {
+        // potentially patch them slowly in the background
+        let alteredStudents = students.map(x => {
+            x.have = false
+            return x
         })
+        console.log("first")
+        await gato(alteredStudents)
+        .then(data=>{
+            console.log(data)
+        })
+        // studentChanger(alteredStudents)
+    }
+
+    async function gato(arr) {
+        let req = null;
+        let updatedList = [];
+        for(let i=0;i<arr.length;i++){
+            // console.log(i)
+            // if(i-2 === arr.length){
+            //     console.log("last one")
+            //     return services.postData(arr[i])
+            // }
+            req = (services.postData(arr[i]))
+            await req.then(res=> console.log(i, res))
+        }
     }
 
     return (
         <>
-            {aGroup ? 
-            <>
-                <button style={{width: '30%', alignSelf: 'center', marginTop: '2%'}} onClick={twoGroupGenerator}>Reshuffle</button>
-                <div id='groupHolder'>
-                    <div>
-                        <h1>Group 1</h1>
-                        <p style={{color: 'blue', fontWeight: 'bold'}}>{aGroup[0].name}</p>
-                        <ul style={{listStyleType: "none", padding: '0%', width: '100%'}}>
-                            {aGroup.map(x=>x !== aGroup[0]?<li>{x.name}</li>: null)}
-                        </ul>
+            {aGroup ?
+                <>
+                    <button style={{ width: '30%', alignSelf: 'center', marginTop: '2%' }} onClick={twoGroupGenerator}>Reshuffle</button>
+                    <div id='groupHolder'>
+                        <div>
+                            <h1>Group 1</h1>
+                            <p style={{ color: 'blue', fontWeight: 'bold' }}>{aGroup[0].name}</p>
+                            <ul style={{ listStyleType: "none", padding: '0%', width: '100%' }}>
+                                {aGroup.map(x => x !== aGroup[0] ? <li>{x.name}</li> : null)}
+                            </ul>
+                        </div>
+                        <div>
+                            <h1>Group 2</h1>
+                            <p style={{ color: 'blue', fontWeight: 'bold' }}>{bGroup[0].name}</p>
+                            <ul style={{ listStyleType: "none", padding: '0%', width: '100%' }}>
+                                {bGroup.map(x => x !== bGroup[0] ? <li>{x.name}</li> : null)}
+                            </ul>
+                        </div>
                     </div>
-                    <div>
-                        <h1>Group 2</h1>
-                        <p style={{color: 'blue', fontWeight: 'bold'}}>{bGroup[0].name}</p>
-                        <ul style={{listStyleType: "none", padding: '0%', width: '100%'}}>
-                            {bGroup.map(x=>x !== bGroup[0]?<li>{x.name}</li>: null)}
-                        </ul>
-                    </div>
-                </div>
-                <button onClick={()=>doubleSwap(aGroup[0], bGroup[0])} style={{width: '38%', alignSelf: 'center'}}>All Done, Thanks</button>
-            </>
-            :
-            <>
-                <h1>Students in Pool</h1>
-                <p>
-                    <span>
-                        <button
-                        className='picker'
-                        onClick={highlighter}
-                        >
-                        PICK A VICTIM
+                    <button onClick={() => doubleSwap(aGroup[0], bGroup[0])} style={{ width: '38%', alignSelf: 'center' }}>All Done, Thanks</button>
+                </>
+                :
+                <>
+                    <h1>Students in Pool</h1>
+                    <p>
+                        <span>
+                            <button
+                                className='picker'
+                                onClick={highlighter}
+                            >
+                                PICK A VICTIM
                         </button>
-                    </span>
-                    <span>
-                        <button
-                        className='picker'
-                        onClick={twoGroupGenerator}
-                        >
-                        Two Group Generator
+                        </span>
+                        <span>
+                            <button
+                                className='picker'
+                                onClick={twoGroupGenerator}
+                            >
+                                Two Group Generator
                         </button>
-                    </span>
-                </p>
-                <ol>
-                    {populateStudents(false)}
-                </ol>
-                <h1>Students who are safe....for now</h1>
-                <ul id='completed'>
-                    {populateStudents(true)}
-                </ul>
-                <button onClick={reset} id='reset'>
-                    reset
-                </button>
-            </>
-            } 
+                        </span>
+                    </p>
+                    <ol>
+                        {populateStudents(false)}
+                    </ol>
+                    <h1>Students who are safe....for now</h1>
+                    <ul id='completed'>
+                        {populateStudents(true)}
+                    </ul>
+                    <button onClick={reset} id='reset'>
+                        reset
+                    </button>
+                </>
+            }
         </>
     )
 }
 
 export default HooksPage
+
 
 
